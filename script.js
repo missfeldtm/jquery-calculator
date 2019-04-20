7a694af60861dcb5e1f17d7f33f2d65e795379b3
Mckenna <missfeldtm@gmail.com>
2019-04-20 12:25:53 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,72 @@
+
+$(document).ready(function() {
+
+
+
+    //get number values
+
+    var button1 = $("#button-1");
+    var button2 = $("#button-2");
+    var button3 = $("#button-3");
+    var button4 = $("#button-4");
+    var button5 = $("#button-5");
+    var button6 = $("#button-6");
+    var button7 = $("#button-7");
+    var button8 = $("#button-8");
+    var button9 = $("#button-9");
+
+    var firstNum = $("#first-number");
+
+    var primaryNum = $(".number");
+
+    primaryNum.click(function(){
+
+        console.log("clicked");
+        var val = primaryNum.val();
+
+    
+        console.log(val);
+
+
+
+    })
+
+        //event listener on click
+
+        //store the clicked value into a variable
+
+        //click an operator
+
+
+            //find and add to object
+
+
+
+    //objects that hold the different functinons
+
+         //add
+
+            //take two numbers
+
+        //subtract
+
+        //multiply
+
+        //divide
+
+        //exponenet
+
+
+
+
+
+
+    //reset button
+        //resets the calculator
+        //sets values to 0
+
+
+    // Your code here...
+    
+    
+    });
